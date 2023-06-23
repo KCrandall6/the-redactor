f69f9a25011af686ebebc1a9b181239c360bd3e8
KCrandall6 <kcrandall67@gmail.com>
2023-06-22 19:14:53 -0700

    updated home page with button logic

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Button } from 'react-bootstrap';
+import Redactor from './Redactor';
 
 const Home = () => {
 
+const [startRedact, setStartRedact] = useState(false);
+
+const onDocSuccess = () => {
+  setStartRedact(!startRedact);
+}
+
   return (
     <>
-      <h1>This is the home page</h1>
+    <div className='d-flex flex-column align-items-center'>
+      {startRedact === false ? (
+        <>
+          <h1>This is the home page</h1>
+          <Button size="lg" onClick={() => onDocSuccess()}>Upload a Document</Button>
+        </>
+      ) : (
+        <Redactor />
+      )}
+    </div>
     </>
   )
 };
---
+++ src/Redactor.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Redactor = () => {
+
+  return (
+    <>
+      <h1>redactor</h1>
+    </>
+  )
+};
+
+export default Redactor;
