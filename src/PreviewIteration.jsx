c89212b3a636468797d0f8203d89a2d71c8fdea0
KCrandall6 <kcrandall67@gmail.com>
2023-07-10 20:07:58 -0700

    updated JSZip formatting to correctly format the xml data

--- src/PreviewIteration.jsx
+++ src/PreviewIteration.jsx
@@ -10,46 +10,56 @@ const PreviewIteration = ({ parsedFile, wordMap, redactFiller }) => {
     const parser = new DOMParser();
     const xmlDoc = parser.parseFromString(parsedFile, 'text/xml');
     const paragraphs = xmlDoc.getElementsByTagName('w:p');
-
+  
     for (let i = 0; i < paragraphs.length; i++) {
       const paragraph = paragraphs[i];
       const textNodes = paragraph.getElementsByTagName('w:t');
-
+  
       for (let j = 0; j < textNodes.length; j++) {
         const textNode = textNodes[j];
         let text = textNode.textContent.trim();
-
+  
         for (const key in wordMap) {
           const wordList = wordMap[key];
-
+  
           for (let k = 0; k < wordList.length; k++) {
             const wordObj = wordList[k];
             const word = wordObj.text;
             const redactFiller = '[redacted]';
             const regex = new RegExp(`\\b${word}\\b`, 'g');
-
+  
             text = text.replace(regex, redactFiller);
           }
         }
-
+  
         textNode.textContent = text;
       }
     }
-
+  
     const serializer = new XMLSerializer();
-    const updatedFile = serializer.serializeToString(xmlDoc);
+    let updatedFile = serializer.serializeToString(xmlDoc);
+  
+    // Find the position of the complete XML declaration
+    const xmlDeclaration = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
+    const index = updatedFile.indexOf(xmlDeclaration) + xmlDeclaration.length;
+  
+    // Insert new line character after the complete XML declaration
+    updatedFile = updatedFile.slice(0, index) + '\n' + updatedFile.slice(index);
+  
     console.log('Updated File:', updatedFile);
     setRedactedFile(updatedFile);
-
   }, [parsedFile, wordMap]);
+  
 
   const saveAsWordDoc = async () => {
     const zip = new JSZip();
     const content = new TextEncoder().encode(redactedFile); // Encode the file as UTF-8
     zip.file('word/document.xml', content);
-
-    const zipContent = await zip.generateAsync({ type: 'blob' });
-    saveAs(zipContent, 'document.docx');
+  
+    zip.generateAsync({ type: 'blob', mimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' })
+      .then(function (blob) {
+        saveAs(blob, 'document.docx');
+      });
   };
 
   return (
