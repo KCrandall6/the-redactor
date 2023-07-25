2c75727aa85a0b579ce575f6118357863f127391
KCrandall6 <kcrandall67@gmail.com>
2023-07-25 15:13:04 -0700

    updated endpoint

--- src/configEndpoint.js
+++ src/configEndpoint.js
@@ -1,6 +1,6 @@
 //file for endpoint so it is only necessary to change here when switching between development and deployment
 
-const endpoint = 'http://localhost:8888';
-// const endpoint = 'https://docredactor.netlify.app/';
+// const endpoint = 'http://localhost:8888';
+const endpoint = 'https://docredactor.netlify.app/';
 
 module.exports = { endpoint };
